0933741d74fbff560b7c37791933e036403c85ab
Sumit Kaushal <sumitkaushal039@gmail.com>
2024-10-04 22:58:36 +0530

    Created Course, OTP, RatingAndReviews, Tags Models All Model Created At this time

---
+++ models/RatingAndReview.js
@@ -0,0 +1,20 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const ratingAndReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("RatingAndReview", ratingAndReviewSchema);
