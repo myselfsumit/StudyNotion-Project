6b864f6c473f3690582aad6d50ba4b89fb5dc1c3
Sumit Kaushal <sumitkaushal039@gmail.com>
2024-10-04 19:37:26 +0530

    Created User and Profile Model

---
+++ models/Profile.js
@@ -0,0 +1,21 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  gender: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: String,
+  },
+  about: {
+    type: String,
+    trim: true,
+  },
+  contactNumber: {
+    type: Number,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model("Profile", profileSchema);
