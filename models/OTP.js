05fb176c41da5f5b635ff973d57ad61c3f14e04c
Sumit Kaushal <sumitkaushal039@gmail.com>
2024-10-04 23:39:34 +0530

    OTP Mailing Logic are also done move to write controller

--- models/OTP.js
+++ models/OTP.js
@@ -17,4 +17,24 @@ const OTPSchema = new mongoose.Schema({
   },
 });
 
+//a function --> to send emails
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verfication mail from StudyNotion",
+      otp
+    );
+    console.log("Email sent successfully", mailResponse);
+  } catch (err) {
+    console.log("Error occured while sending mails: ", err);
+    throw error;
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+});
+
 module.exports = mongoose.model("OTP", OTPSchema);
