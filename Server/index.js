9216dc9982d14d16d85cd9a77b49303c99e5c83b
Sumit Kaushal <sumitkaushal039@gmail.com>
2024-10-27 09:43:26 +0530

    Refactor Code

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,7 @@ const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
 
+
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
